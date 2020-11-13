99f89e66edec3a79703a6e3eb796a752f6f736f2
shotpoet86 <curlybracemaster@gmail.com>
2020-11-13 15:16:17 -0500

    added infinite scroll functionality

--- script.js
+++ script.js
@@ -1,12 +1,29 @@
 const imageContainer = document.getElementById('image-container');
 const loader = document.getElementById('loader');
 
+let ready = false;
+let imagesLoaded = 0;
+let totalImages = 0;
 let photosArray = [];
-
+let isInitialLoadedImages = true;
 /*unsplash api*/
-const count = 10;
+let initialImageCount = 5;
 const apiKey = 'x--y8k_PgC9QSzzBpdGcoXs_dJ6bqaOBb9imE3kgDYw';
-const apiUrl = `https://api.unsplash.com/photos/?client_id=${apiKey}&count=${count}`;
+let apiUrl = `https://api.unsplash.com/photos/?client_id=${apiKey}&initialImageCount=${initialImageCount}`;
+
+
+function updateAPIURLWithNewCount() {
+    apiUrl = `https://api.unsplash.com/photos/?client_id=${apiKey}&initialImageCount=${initialImageCount}`;
+}
+
+/*checks if all images loaded*/
+function imageLoaded() {
+    imagesLoaded++;
+    if (imagesLoaded === totalImages) {
+        ready = true;
+        loader.hidden = true;
+    }
+}
 
 /*helper function to set attributes on DOM elements*/
 function setAttributes(element, attributes) {
@@ -17,6 +34,8 @@ function setAttributes(element, attributes) {
 
 /*create elements for links & photos. Add to DOM*/
 function displayPhotos() {
+    imagesLoaded = 0;
+    totalImages = photosArray.length;
     /*runs function for each object in photosArray*/
     photosArray.forEach((photo) => {
         /*creates anchor to link to Unsplash*/
@@ -32,7 +51,10 @@ function displayPhotos() {
             alt: photo.alt_description,
             title: photo.alt_description
         });
-        /*puts image inside anchor , then puts both inide imageContainer element*/
+        /*event listener, checks when each is image is finished loading*/
+        img.addEventListener('load', imageLoaded())
+
+        /*puts image inside anchor , then puts both inside imageContainer element*/
         item.appendChild(img);
         imageContainer.appendChild(item);
     });
@@ -44,10 +66,22 @@ async function getPhotos() {
         const response = await fetch(apiUrl);
         photosArray = await response.json();
         displayPhotos();
+        if (isInitialLoadedImages) {
+            updateAPIURLWithNewCount(30);
+            isInitialLoadedImages = false;
+        }
     } catch (error) {
         /*catch error*/
     }
 }
 
+/*check to see if scrolling is nearing bottom of page, if so load more photos from API*/
+window.addEventListener('scroll', () => {
+    if (window.innerHeight + window.scrollY >= document.body.offsetHeight - 1000 && ready) {
+        ready = false;
+        getPhotos();
+    }
+});
+
 /*on load*/
-console.log(getPhotos());
+getPhotos();
